0714d7176b7faf31b235c5e2ad970604f49439ec
danthesane <danthesane@gmail.com>
2016-03-22 16:17:05 -0400

    Wait wait
    @jart there we go

--- Problem1.java
+++ Problem1.java
@@ -6,13 +6,11 @@ import java.util.ArrayList;
 public class Problem1 {
 
   public static void main(String[] args) {
-    int i = 1;
     int total = 0;
-    while (i < 1000) {
+    for (int i = 1; i < 1000; i++) {
       if (i % 3 == 0 || i % 5 == 0 ) { 
         total += i;
       }
-      i++;
     }
     System.out.printf("\nSum total of multiples of 3 and 5 beneath 1000: %d", total);
   }
