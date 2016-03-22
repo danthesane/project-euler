83fe4036fc3e00e8d6aa135b45584de9d464ccc7
danthesane <danthesane@gmail.com>
2016-03-22 15:53:27 -0400

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,5 +1,5 @@
-//Project Euler problem 1
-//Print the sum of all multiples of 3 and 5 beneath 1000
+// Project Euler problem 1
+// Print the sum of all multiples of 3 and 5 beneath 1000
 
 import java.util.ArrayList;
 
@@ -8,31 +8,26 @@ public class Problem1 {
   public static void main(String[] args) {
     int i = 1;
     int mult = 3;
-    ArrayList<Integer> thoseMults = new ArrayList<Integer> (0);
-    while(mult < 1000) {
+    ArrayList<Integer> thoseMults = new ArrayList<Integer>(0);
+    while (mult < 1000) {
       thoseMults.add(mult);
-      System.out.print(mult);
-      i = i + 1;
+      i++;
       mult = 3 * i;
     }
     
     i = 0;
     mult = 5;
-    while(mult < 1000) {
-      if (mult % 3 == 0) {
-      }
-      else {
+    while (mult < 1000) {
+      if (mult % 3 != 0) {
         thoseMults.add(mult);
-        System.out.print(mult);
       }
-      i = i + 1;
+      i++;
       mult = 5 * i;
     }
     int total = 0;
-    for ( Integer nums : thoseMults){
+    for ( Integer nums : thoseMults) {
       total = total + nums;
     }
-    System.out.print("\nSum total of multiples of 3 and 5 beneath 1000: ");
-    System.out.println(total);
+    System.out.printf("\nSum total of multiples of 3 and 5 beneath 1000: %d", total);
   }
 }
